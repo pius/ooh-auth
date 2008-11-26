22040d2fea61856b43c7f002d549de29b74461ca
Dan <dan@angryamoeba.co.uk>
2008-11-26 16:40:22 +0000

    adding notes to auth model

--- app/models/authentication/dm_authentication.rb
+++ app/models/authentication/dm_authentication.rb
@@ -1,7 +1,16 @@
+=begin
+ Authentication model
+ 
+ An Authentication is a single example of permissions given by a user to an authenticating application
+ to act on his or her behalf. Each Authentication must relate to a user, and an authenticating application.
+ There will be only one Authentication per application per user.
+ 
+ During the authorisation process an object called a 'receipt' is created - the receipt is a key which can
+ be used to retrieve a proper Authentication token.
+=end
+
 class MerbAuthSliceFullfat::Authentication
   include DataMapper::Resource
   
   property :id, Serial
-
-
 end
